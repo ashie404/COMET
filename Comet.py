19b319be1452affbdebfd12bebfeb6b8e13fda98
June <22226164+juniebyte@users.noreply.github.com>
2023-06-01 20:04:12 -0500

    functional handshake with stardriver devices

--- Comet.py
+++ Comet.py
@@ -7,17 +7,59 @@ import mido
 import serial
 import serial.tools.list_ports
 import os.path
+import threading
 
 _serial = serial.Serial()
+max_dev_addr = 0
+
+waiting_serial = False
 
 def openSerialPort(p):
     #open serial port
     global _serial
-    _serial = serial.Serial(port=p, baudrate=115200)
+    global max_dev_addr
+    global current_device
+    global waiting_serial
+    _serial = serial.Serial(port=p,baudrate=115200,parity=serial.PARITY_ODD,timeout=2)
 
     #send ping to device to see if we're actually communicating with STARDRIVER or some random serial device
-    _serial.write({0xE6, 0xFF, 0x01, 0x01}) # E6 - start byte, FF - device addr 255/all devices, 01 - length of message (1 byte), 01 - ping control byte
-
+    _serial.write([0xE6, 0xFF, 0x01, 0x01]) # E6 - start byte, FF - device addr 255/all devices, 01 - length of message (1 byte), 01 - ping control byte
+    print('Ping sent, waiting for device pong...')
+    # wait to recieve pong message
+    waiting_serial = True
+    msg_pos = 0
+    while waiting_serial:
+        if _serial.readable:
+            if msg_pos == 0:
+                if _serial.read() == b'\xe6': #if start byte, advance msg pos, if not, cancel handshake.
+                    msg_pos = 1
+                else:
+                    print('Invalid start byte, handshake cancelled.')
+                    waiting_serial = False
+            elif msg_pos == 1:
+                _serial.read() # 2nd byte doesn't really matter, this is device address, still gotta read it tho
+                msg_pos = 2
+            elif msg_pos == 2:
+                # 3rd byte is message size. this should always be 0x02 for a pong message. if not, cancel handshake.
+                if _serial.read() == b'\x02':
+                    msg_pos = 3
+                else:
+                    print('Invalid message size, handshake cancelled.')
+                    waiting_serial = False
+            elif msg_pos == 3:
+                # 4th byte is the pong control byte, which is also always 0x02. 5th byte gives us the max device address for the connected board
+                if _serial.read() == b'\x02':
+                    print('Pong recieved!')
+                    # successfully connected to a stardriver device
+                    max_dev_addr = _serial.read()
+                    current_device.config(text='Current device: ' + p + ', Max inst addr: ' + str(int.from_bytes(max_dev_addr, 'little')))
+                    # send reset to ensure all instruments are in a resting state
+                    _serial.write([0xE6, 0xFF, 0x01, 0xFF])
+                    waiting_serial = False #handshake complete
+                else:
+                    print('Invalid control byte, handshake cancelled.')
+                    waiting_serial = False #cancel handshake, message was not a pong message
+                    
 
 def updateSerialDevices():
     #reset serial devices frame
@@ -27,21 +69,38 @@ def updateSerialDevices():
     df.grid(column=1, row=0, padx=15, sticky=tk.N)
     _ports = serial.tools.list_ports.comports()
     for port in _ports:
-        _pbtn = ttk.Radiobutton(df, text=port.name, variable=port_sel, command=openSerialPort(port.name))
+        _pbtn = ttk.Radiobutton(df, text=port.name, variable=port_sel, command=lambda: openSerialPort(port.name))
         _pbtn.pack(anchor=tk.NW)
 
 def stop():
     #stop playback
+    global _playing
+    global play_btn
     _playing = False
+    play_btn.config(image=_playimg)
 
 def playMidi():
+    global _playing
+    global play_btn
     _playing = True
-    for msg in _midi.play():
-        print(msg)
-        # TODO: implement midi play functionality
+    play_btn.config(image=_pauseimg)
+    player = threading.Thread(target=midiLoop)
+    player.start()
     _playing = False
+    play_btn.config(image=_playimg)
+
+def midiLoop():
+    global _midi
+    global _playing
+    for msg in _midi.play():
+        if not _playing: # if player state has changed, exit midi playing loop
+            break
+        print(msg)
+        # TODO: implement midi playing functionality
 
 def openFile():
+    global song_name
+    global _midi
     _types = (
         ('MIDI files', '*.mid *.midi'),
         ('All files', '*.*')
@@ -53,7 +112,6 @@ def openFile():
         filetypes=_types
     )
 
-    global song_name
     song_name.config(text=os.path.basename(_path))
     _midi = mido.MidiFile(_path)
 
@@ -71,8 +129,8 @@ port_sel = tk.StringVar()
 main_frame = ttk.Frame(root)
 main_frame.grid(column=0, row=0, padx=5, pady=5, sticky=tk.NSEW)
 
-pf = ttk.Frame(main_frame)
-pf.grid(column=0, row=0, sticky=tk.N)
+pf = ttk.LabelFrame(main_frame,text='Player')
+pf.grid(column=0, row=0, padx=5, pady=5, sticky=tk.NW)
 
 # setup player section
 song_name = Label(pf, text='No song loaded')
@@ -104,5 +162,8 @@ stop_btn.grid(column=2, row=0, sticky=tk.W)
 df = ttk.LabelFrame(main_frame, text='Serial Devices')
 
 updateSerialDevices()
+
+current_device = Label(main_frame, text="Current device: None, Max inst. addr: N/A")
+current_device.grid(column=0, row=1, padx=5, pady=5, sticky=tk.SW)
 
 root.mainloop()
